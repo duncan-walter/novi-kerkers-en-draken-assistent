9d57bc055716f5ac7b86e690a6472a530159474c
Duncan Walter <duncan1997@hotmail.nl>
2025-06-17 09:30:34 +0200

    added slider to easily adjust form control width while developing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 
 // Framework dependencies
+import {useState} from "react";
 import {useForm} from "react-hook-form";
 
 // Components
@@ -19,6 +20,7 @@ function App() {
     formState: {errors},
     register
   } = useForm();
+  const [formWidth, setFormWidth] = useState("500");
 
   function handleFormSubmit(data) {
     console.log(data);
@@ -26,147 +28,154 @@ function App() {
 
   return (
     <main>
-      <h1>🛠️ Form control testing zone 🚧</h1>
+      <div className="form-container">
+        <h1>🛠️ Form control testing zone 🚧</h1>
 
-      <form onSubmit={handleSubmit(handleFormSubmit)}>
-        <NumberFormControl
-          id="numberFormControl"
-          name="numberFormControl"
-          label="Number"
-          register={register}
-          error={errors.numberFormControl}
-          validationRules={{
-            required: true,
-            minimumValue: 0,
-            maximumValue: 10
-          }}
-        />
-        <NumberFormControl
-          id="numberFormControlWithPlaceholder"
-          name="numberFormControlWithPlaceholder"
-          label="Number with placeholder"
-          placeholder="0"
-          register={register}
-          error={errors.numberFormControlWithPlaceholder}
-        />
+        <div className="form-control-controls">
+          <p>Adjust form control length:</p>
+          <input type="range" min="100" max="1250" onChange={e => setFormWidth(e.target.value)}/>
+        </div>
 
-        <PasswordFormControl
-          id="passwordFormControl"
-          name="passwordFormControl"
-          label="Password"
-          register={register}
-          error={errors.passwordFormControl}
-          validationRules={{
-            required: true,
-            minimumLength: 8,
-            maximumLength: 255
-          }}
-        />
+        <form onSubmit={handleSubmit(handleFormSubmit)} style={{width: formWidth + "px"}}>
+          <NumberFormControl
+            id="numberFormControl"
+            name="numberFormControl"
+            label="Number"
+            register={register}
+            error={errors.numberFormControl}
+            validationRules={{
+              required: true,
+              minimumValue: 0,
+              maximumValue: 10
+            }}
+          />
+          <NumberFormControl
+            id="numberFormControlWithPlaceholder"
+            name="numberFormControlWithPlaceholder"
+            label="Number with placeholder"
+            placeholder="0"
+            register={register}
+            error={errors.numberFormControlWithPlaceholder}
+          />
 
-        <SearchFormControl
-          id="searchFormControl"
-          name="searchFormControl"
-          label="Search"
-          register={register}
-          error={errors.searchFormControl}
-          validationRules={{
-            required: true,
-            minimumLength: 0,
-            maximumLength: 50
-          }}
-        />
-        <SearchFormControl
-          id="searchFormControlWithPlaceholder"
-          name="searchFormControlWithPlaceholder"
-          label="Search with placeholder"
-          placeholder="enter search term"
-          register={register}
-          error={errors.searchFormControlWithPlaceholder}
-        />
+          <PasswordFormControl
+            id="passwordFormControl"
+            name="passwordFormControl"
+            label="Password"
+            register={register}
+            error={errors.passwordFormControl}
+            validationRules={{
+              required: true,
+              minimumLength: 8,
+              maximumLength: 255
+            }}
+          />
 
-        <SelectFormControl
-          id="selectFormControl"
-          name="selectFormControl"
-          label="Select"
-          options={[
-            {value: "1", label: "value 1"},
-            {value: "2", label: "value 2"},
-            {value: "3", label: "value 3"}
-          ]}
-          showPlaceholder={false}
-          register={register}
-          error={errors.selectFormControl}
-        />
-        <SelectFormControl
-          id="selectFormControlWithPlaceholder"
-          name="selectFormControlWithPlaceholder"
-          label="Select with placeholder"
-          options={[
-            {value: "1", label: "value 1"},
-            {value: "2", label: "value 2"},
-            {value: "3", label: "value 3"}
-          ]}
-          register={register}
-          error={errors.selectFormControlWithPlaceholder}
-          validationRules={{
-            required: true
-          }}
-        />
+          <SearchFormControl
+            id="searchFormControl"
+            name="searchFormControl"
+            label="Search"
+            register={register}
+            error={errors.searchFormControl}
+            validationRules={{
+              required: true,
+              minimumLength: 0,
+              maximumLength: 50
+            }}
+          />
+          <SearchFormControl
+            id="searchFormControlWithPlaceholder"
+            name="searchFormControlWithPlaceholder"
+            label="Search with placeholder"
+            placeholder="enter search term"
+            register={register}
+            error={errors.searchFormControlWithPlaceholder}
+          />
 
-        <SliderFormControl
-          id="sliderFormControl"
-          name="sliderFormControl"
-          label="Slider"
-          minimumValue={0}
-          maximumValue={30}
-          register={register}
-          error={errors.sliderFormControl}
-        />
+          <SelectFormControl
+            id="selectFormControl"
+            name="selectFormControl"
+            label="Select"
+            options={[
+              {value: "1", label: "value 1"},
+              {value: "2", label: "value 2"},
+              {value: "3", label: "value 3"}
+            ]}
+            showPlaceholder={false}
+            register={register}
+            error={errors.selectFormControl}
+          />
+          <SelectFormControl
+            id="selectFormControlWithPlaceholder"
+            name="selectFormControlWithPlaceholder"
+            label="Select with placeholder"
+            options={[
+              {value: "1", label: "value 1"},
+              {value: "2", label: "value 2"},
+              {value: "3", label: "value 3"}
+            ]}
+            register={register}
+            error={errors.selectFormControlWithPlaceholder}
+            validationRules={{
+              required: true
+            }}
+          />
 
-        <TextareaFormControl
-          id="textareaFormControl"
-          name="textareaFormControl"
-          label="Textarea"
-          register={register}
-          error={errors.textareaFormControl}
-          validationRules={{
-            required: true,
-            minimumLength: 10,
-            maximumLength: 500
-          }}
-        />
-        <TextareaFormControl
-          id="textareaFormControlWithPlaceholder"
-          name="textareaFormControlWithPlaceholder"
-          label="Textarea with placeholder"
-          placeholder="enter textarea"
-          register={register}
-          error={errors.textareaFormControlWithPlaceholder}
-        />
+          <SliderFormControl
+            id="sliderFormControl"
+            name="sliderFormControl"
+            label="Slider"
+            minimumValue={0}
+            maximumValue={30}
+            register={register}
+            error={errors.sliderFormControl}
+          />
 
-        <TextFormControl
-          id="textControl"
-          name="textControl"
-          label="Text"
-          register={register}
-          error={errors.textControl}
-          validationRules={{
-            required: true,
-            minimumLength: 8,
-            maximumLength: 50
-          }}
-        />
-        <TextFormControl
-          id="textControlWithPlaceholder"
-          name="textControlWithPlaceholder"
-          label="Text with placeholder"
-          placeholder="enter text"
-          register={register}
-          error={errors.textControlWithPlaceholder}
-        />
+          <TextareaFormControl
+            id="textareaFormControl"
+            name="textareaFormControl"
+            label="Textarea"
+            register={register}
+            error={errors.textareaFormControl}
+            validationRules={{
+              required: true,
+              minimumLength: 10,
+              maximumLength: 500
+            }}
+          />
+          <TextareaFormControl
+            id="textareaFormControlWithPlaceholder"
+            name="textareaFormControlWithPlaceholder"
+            label="Textarea with placeholder"
+            placeholder="enter textarea"
+            register={register}
+            error={errors.textareaFormControlWithPlaceholder}
+          />
 
-        <button type="submit">Submit</button>
-      </form>
+          <TextFormControl
+            id="textControl"
+            name="textControl"
+            label="Text"
+            register={register}
+            error={errors.textControl}
+            validationRules={{
+              required: true,
+              minimumLength: 8,
+              maximumLength: 50
+            }}
+          />
+          <TextFormControl
+            id="textControlWithPlaceholder"
+            name="textControlWithPlaceholder"
+            label="Text with placeholder"
+            placeholder="enter text"
+            register={register}
+            error={errors.textControlWithPlaceholder}
+          />
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </main>
   );
 }
