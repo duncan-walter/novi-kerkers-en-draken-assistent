9a4180be39d6a77b63599b948bf177c0dffe7eda
Duncan Walter <duncan1997@hotmail.nl>
2025-07-19 16:22:24 +0200

    added getWeaponInformationByIndex to weaponInformationService

--- src/services/weaponInformationService.js
+++ src/services/weaponInformationService.js
@@ -5,16 +5,22 @@ import dnd5eClient from '../clients/dnd5eClient.js';
 import {requestWithCache} from '../helpers/requestHelpers.js';
 import {weaponInformationKey} from '../constants/localStorageKeys.js';
 
-// Local constants
-const client = dnd5eClient;
-const endpoint = 'equipment-categories/weapon';
-
 const weaponInformationService = {
   getWeaponInformationIndex: (options = {useCache: false}) => {
-    let request = (_, signal) => client.get(endpoint, {signal});
+    let request = (_, signal) => dnd5eClient.get('equipment-categories/weapon', {signal});
 
     if (options.useCache) {
       request = requestWithCache(request, `${weaponInformationKey}:index`, 3600);
+    }
+
+    return request;
+  },
+
+  getWeaponInformationByIndex: (index, options = {useCache: false}) => {
+    let request = (_, signal) => dnd5eClient.get(`equipment/${index}`, {signal});
+
+    if (options.useCache) {
+      request = requestWithCache(request, `${weaponInformationKey}:${index}`, 3600);
     }
 
     return request;
