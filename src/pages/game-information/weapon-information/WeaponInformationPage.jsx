864e54a43718a2f338731a3fd26af016f1658530
Duncan Walter <duncan1997@hotmail.nl>
2025-07-19 16:54:12 +0200

    implemented simple render of individual weapon information

--- src/pages/game-information/weapon-information/WeaponInformationPage.jsx
+++ src/pages/game-information/weapon-information/WeaponInformationPage.jsx
@@ -1,13 +1,97 @@
 // Styling
 import './WeaponInformationPage.css';
 
+// Framework dependencies
+import {useEffect} from "react";
+import {useParams} from "react-router-dom";
+
+// Custom hooks
+import useRequestState from "../../../hooks/useRequestState.js";
+
+// Services
+import weaponInformationService from "../../../services/weaponInformationService.js";
+
 // Components
 import Panel from "../../../components/ui/Panel/Panel.jsx";
+import Spinner from "../../../components/ui/Spinner/Spinner.jsx";
 
 function WeaponInformationPage() {
-  return (
-    <Panel title="Dynamisch bepaald o.b.v. wapen">
+  const params = useParams();
 
+  const {data: weapon, loading, executeRequest} = useRequestState(
+    weaponInformationService.getWeaponInformationByIndex(params.id, {useCache: true}),
+    {executeOnMount: false, isAbortable: true}
+  );
+
+  useEffect(() => {
+    if (params.id) {
+      executeRequest();
+    }
+  }, [params.id]);
+
+  return (
+    <Panel title={weapon && weapon.name}>
+      {loading && (
+        <Spinner size="large"/>
+      )}
+
+      {!loading && weapon && (
+        <div className="weapon-information">
+          <div className="weapon-information__row">
+            <dl className="weapon-information__column">
+              <div>
+                <dt>Damage</dt>
+                <dd>
+                  <div>{weapon.damage['damage_dice']} ({weapon.damage['damage_type'].name})</div>
+
+                  {weapon['two_handed_damage'] && (
+                    <div>
+                      {weapon['two_handed_damage']['damage_dice']} ({weapon['two_handed_damage']['damage_type'].name}) (2h)
+                    </div>
+                  )}
+                </dd>
+              </div>
+
+              <div>
+                <dt>Bereik</dt>
+                <dd>{weapon.range.normal}ft{weapon.range.long ? ` / ${weapon.range.long}ft` : ''}</dd>
+              </div>
+
+              {weapon['throw_range'] && (
+                <div>
+                  <dt>Gooi bereik</dt>
+                  <dd>{weapon['throw_range'].normal}/{weapon['throw_range'].long}</dd>
+                </div>
+              )}
+            </dl>
+
+            <dl className="weapon-information__column">
+              <div>
+                <dt>Wapen categorie</dt>
+                <dd>{weapon['category_range']}</dd>
+              </div>
+
+              <div>
+                <dt>Eigenschappen</dt>
+                <dd>
+                  <ul>
+                    {weapon.properties.map(property =>
+                      <li key={property.name}>{property.name}</li>
+                    )}
+                  </ul>
+                </dd>
+              </div>
+            </dl>
+          </div>
+
+          <dl className="weapon-information__row-end">
+            <div>
+              <dt>Waarde</dt>
+              <dd>{weapon.cost.quantity}{weapon.cost.unit}</dd>
+            </div>
+          </dl>
+        </div>
+      )}
     </Panel>
   );
 }
