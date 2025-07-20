236f8e0f0bb9f27a7faa46003fb5b54bae1edba1
duncan-walter <duncan1997@hotmail.nl>
2025-07-20 16:23:21 +0200

    added favorite support to SearchResultItem component

--- src/components/ui/SearchResultItem/SearchResultItem.jsx
+++ src/components/ui/SearchResultItem/SearchResultItem.jsx
@@ -1,12 +1,25 @@
+// Styling
 import './SearchResultItem.css';
 
-function SearchResultItem({label, onClick}) {
+// Icons
+import {StarIcon} from "@phosphor-icons/react";
+
+function SearchResultItem({label, onClick, isFavorite, onFavoriteClick}) {
   return (
     <div
       className="search-result-item"
       onClick={onClick}
     >
       <p className="search-result-item__label">{label}</p>
+
+      {onFavoriteClick && (
+        <span
+          className={`search-result-item__favorite ${!isFavorite ? 'search-result-item__favorite-inactive' : ''}`}
+          onClick={onFavoriteClick}
+        >
+          <StarIcon size="18" weight={isFavorite ? 'fill' : 'regular'}/>
+        </span>
+      )}
     </div>
   )
 }
--- src/constants/localStorageKeys.js
+++ src/constants/localStorageKeys.js
@@ -8,3 +8,4 @@ export const characterPossessionsKey = 'characterPossessions';
 export const characterConditionsKey = 'characterConditions';
 export const weaponInformationKey = 'weaponInformation';
 export const monsterInformationKey = 'monsterInformation';
+export const gameInformationFavoritesKey = 'gameInformationFavorites';
