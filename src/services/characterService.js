bc64ef51eeb903b9c4eaa807f8fcfb61219fd1d7
Duncan Walter <duncan1997@hotmail.nl>
2025-07-21 15:49:29 +0200

    updated characters retrieval to only display the characters of the current user

--- src/services/characterService.js
+++ src/services/characterService.js
@@ -1,9 +1,14 @@
 // Custom hooks
 import userDataClient from '../clients/userDataClient.js';
 
+// Framework dependencies
+import {jwtDecode} from "jwt-decode";
+
+
 // Helpers and constants
 import {requestWithCache} from '../helpers/requestHelpers.js';
-import {charactersKey} from '../constants/localStorageKeys.js';
+import {getLocalStorageItem} from "../helpers/localStorageHelpers.js";
+import {charactersKey, userKey} from '../constants/localStorageKeys.js';
 
 // Local constants
 const client = userDataClient;
@@ -12,10 +17,11 @@ const endpoint = 'characters';
 // TODO: Find a way to remove the payload and signal passing, currently this is needed to support the useAbortableRequest hook.
 const characterService = {
   getCharacters: (options = {useCache: false}) => {
-    let request = (_, signal) => client.get(endpoint, {signal});
+    const userId = jwtDecode(getLocalStorageItem(userKey).token).userId;
+    let request = (_, signal) => client.get(`users/${userId}/${endpoint}`, {signal});
 
     if (options.useCache) {
-      request = requestWithCache(request, charactersKey, 60);
+      request = requestWithCache(request, `users/${userId}/${charactersKey}`, 60);
     }
 
     return request;
