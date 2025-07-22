9704ab5eca27e93be70eb7a58ddea4416fa370a5
Duncan Walter <duncan1997@hotmail.nl>
2025-07-22 13:25:33 +0200

    setup vite project for path alias usage

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@clients':  path.resolve(__dirname, './src/clients'),
+      '@components':  path.resolve(__dirname, './src/components'),
+      '@constants':  path.resolve(__dirname, './src/constants'),
+      '@contexts':  path.resolve(__dirname, './src/contexts'),
+      '@helpers':  path.resolve(__dirname, './src/helpers'),
+      '@hooks':  path.resolve(__dirname, './src/hooks'),
+      '@pages':  path.resolve(__dirname, './src/pages'),
+      '@services':  path.resolve(__dirname, './src/services')
+    }
+  },
   plugins: [react(), svgr()],
 })
